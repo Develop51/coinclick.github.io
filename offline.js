1d1d4bc009267461efed35d93dffb12dd7429fdd
Develop51 <53274787+Develop51@users.noreply.github.com>
2019-07-24 21:46:13 +0300

    Файлы Coin Click

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1563993642,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/click-sheet0.png",
+		"images/top-sheet0.png",
+		"images/perevod-sheet0.png",
+		"images/shop-sheet0.png",
+		"images/strelka-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/video-sheet0.png",
+		"images/supercomp-sheet0.png",
+		"images/server-sheet0.png",
+		"images/stoyka_videocard-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/cursor-sheet0.png",
+		"images/videocard-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
